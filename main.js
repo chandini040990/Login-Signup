f56154306eae31bb2d0e17f1cb90bb787eb6500f
Chandini <chandini@Chandinis-MacBook-Air.local>
2024-11-13 18:54:21 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+let feedbackEl = "";
+let feedbackEl2 = "";
+
+function valsignForm() {
+    const signName = document.getElementById("name").value;
+    const signUsername = document.getElementById("username").value;
+    const signEmail = document.getElementById("email").value;
+    const signPassword = document.getElementById("password").value;
+    const feedbackEl = document.getElementById("feedback1");
+
+
+    if (signName && signUsername && signEmail && signPassword !== "") {
+        feedbackEl.innerText = "Signup Successful"
+    }
+    else {
+        feedbackEl.innerText = "Signup Failed! Enter all the values"
+    }
+}
+
+function vallogForm() {
+    const logUsrname = document.getElementById("username2").value;
+    const logPwd = document.getElementById("password2").value;
+    const feedbackEl2 = document.getElementById("feedback2");
+
+
+    if (logUsrname && logPwd !== "") {
+        feedbackEl2.innerText = "Login Successful"
+    }
+    else {
+        feedbackEl2.innerText = "Login Failed! Enter all the values"
+    }
+}
